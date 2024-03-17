60790a8d360502ed0fa5f3b3157ff30118d0b2e2
hyungjae.lee <dlgudwo11@gmail.com>
2024-03-17 12:06:37 +0900

    feat - Spring Server Sent Event
    - init commit

---
+++ springSSE/src/test/java/com/springsse/SpringSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
