b8decb955d2098ded35cbb7f701d95f4aa2d4d21
hyungjae.lee <dlgudwo11@gmail.com>
2024-03-17 15:53:56 +0900

    feat - Spring Boot Gzip
    - init commit

---
+++ springgzip/src/test/java/com/springgzip/SpringgzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springgzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringgzipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
